5dda6f72797d190bd174c41ae1599e1e652ef34d
Labhesh <labheshnikam1991@gmail.com>
2022-01-31 00:23:12 +0530

    Added documentation

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const helmet = require("helmet")
 const cors = require("cors")
 const xss = require("xss-clean")
 
+// Swagger
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
+
 const express = require("express")
 const app = express()
 
@@ -24,10 +29,11 @@ app.use(xss())
 //routes
 app.get("/", (req, res) => {
   res.send(
-    "<h1>Store API</h1><a href='/api/v1/products'>Products route</a><a href='/api-docs'>Documentation</a>"
+    "<h1>Store API</h1><a href='/api/v1/products'>Products route</a><a href='/api-docs'> Documentation</a>"
   )
 })
 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use("/api/v1/products", productsRouter)
 
 //product routes
