273a3cc0a96b8ae91eb7c17cf30d8aa85c0c7d0f
Labhesh <labheshnikam1991@gmail.com>
2022-01-31 00:12:21 +0530

    added security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config()
 require("express-async-errors")
 
+// extra security packages
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
 const express = require("express")
 const app = express()
 
@@ -12,6 +18,9 @@ const errorMiddleware = require("./middleware/error-handler")
 
 //middleware
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 //routes
 app.get("/", (req, res) => {
