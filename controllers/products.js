32b8745382aa6992e061dc952a0f494c958b907d
Labhesh <labheshnikam1991@gmail.com>
2022-01-30 23:57:20 +0530

    Initial commit

---
+++ controllers/products.js
@@ -0,0 +1,74 @@
+const Products = require("../models/product")
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Products.find({}).sort("name")
+
+  res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query
+
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" }
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+      ">": "$gt",
+      ">=": "$gte",
+    }
+
+    const regEx = /\b(<|<=|=|>|>=)\b/g
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    )
+
+    const options = ["price", "rating"]
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-")
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
+
+  console.log(queryObject)
+
+  let results = Products.find(queryObject)
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ")
+    results = results.sort(sortList)
+  } else {
+    results = results.sort("createdAt")
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ")
+    results = results.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  results = results.skip(skip).limit(limit)
+
+  const products = await results
+  res.status(200).json({ products, nbHits: products.length })
+}
+
+module.exports = { getAllProducts, getAllProductsStatic }
